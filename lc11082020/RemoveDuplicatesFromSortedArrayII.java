ab5af775d13c4137859ab21fd16a6929c36ac369
u0966537 <qq928090385@gmail.com>
2020-11-08 16:06:11 -0700

    finished problem 80

--- lc11082020/RemoveDuplicatesFromSortedArrayII.java
+++ lc11082020/RemoveDuplicatesFromSortedArrayII.java
@@ -2,13 +2,15 @@ package lc11082020;
 
 public class RemoveDuplicatesFromSortedArrayII {
 	public int removeDuplicates(int[] nums) {
-		int i = 1;
-		for (int j = 1; j < nums.length; j++) {
-			if (i < 2 || nums[j] > nums[i - 2]) {
-				nums[i++] = nums[j];
+		// start at 1 since duplicates can exist most twice/
+		int res = 1;
+		for (int i = 1; i < nums.length; i++) {
+			// always compare the current pointer's previous 2 index element.
+			if (res < 2 || nums[i] > nums[res - 2]) {
+				nums[res++] = nums[i];
 			}
 		}
-		return i;
+		return res;
 
 	}
 }
