6ed8b539a3404826147f65a0e83bd4cb08822adc
u0966537 <qq928090385@gmail.com>
2020-11-09 23:35:23 -0700

    finished problem 844

--- lc11092020/BackspaceStringCompare.java
+++ lc11092020/BackspaceStringCompare.java
@@ -2,10 +2,8 @@ package lc11092020;
 
 import java.util.Stack;
 public class BackspaceStringCompare {
-	public static void main(String[]args) {
-		backspaceCompare("abcd", "bbcd");
-	}
-    public static boolean backspaceCompare(String S, String T) {
+
+    public boolean backspaceCompare(String S, String T) {
         Stack<Character> s = new Stack<>();
         Stack<Character> t = new Stack<>();
         
@@ -35,18 +33,7 @@ public class BackspaceStringCompare {
         	}
         }
         
-        if(s.size() != t.size()) {
-        	return false;
-        }
-        else {
-        	length = s.size();
-        	for(int i = 0; i < length; i++) {
-        		if(!s.pop().equals(t.pop())) {
-        			return false;
-        		}
-        	}
-        	return true;
-        }
+        return s.toString().equals(t.toString());
     }
     
     
@@ -54,24 +41,46 @@ public class BackspaceStringCompare {
         int i = S.length() - 1, j = T.length() - 1;
         int skipS = 0, skipT = 0;
 
-        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
-            while (i >= 0) { // Find position of next possible char in build(S)
-                if (S.charAt(i) == '#') {skipS++; i--;}
-                else if (skipS > 0) {skipS--; i--;}
-                else break;
+        while (i >= 0 || j >= 0) { 
+        	
+            while (i >= 0) { 
+                if (S.charAt(i) == '#') {
+                	skipS++; 
+                	i--;
+                	}
+                else if (skipS > 0) {
+                	skipS--; 
+                	i--;
+                }
+                else {
+                	break;
+                }
             }
-            while (j >= 0) { // Find position of next possible char in build(T)
-                if (T.charAt(j) == '#') {skipT++; j--;}
-                else if (skipT > 0) {skipT--; j--;}
-                else break;
+            
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                	skipT++; 
+                	j--;
+                }
+                else if (skipT > 0) {
+                	skipT--; 
+                	j--;
+                }
+                else {
+                	break;
+                }
             }
             // If two actual characters are different
-            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j))
+            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j)) {
                 return false;
-            // If expecting to compare char vs nothing
-            if ((i >= 0) != (j >= 0))
+            }
+            // If one index is positive, and another index is negative
+            if ((i >= 0) != (j >= 0)) {
                 return false;
-            i--; j--;
+            }
+            
+            i--; 
+            j--;
         }
         return true;
     }
