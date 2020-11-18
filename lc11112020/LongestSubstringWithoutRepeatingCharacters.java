d6d80819cadfd20f1ccd90a873cd37129045970a
u0966537 <qq928090385@gmail.com>
2020-11-17 23:28:55 -0700

    finished problem 3

--- lc11112020/LongestSubstringWithoutRepeatingCharacters.java
+++ lc11112020/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,6 +1,8 @@
 package lc11112020;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
@@ -12,7 +14,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
 		Set<Character> set = new HashSet<Character>();
 		int res = 0, start = 0, end = 0;
 
-		while (res < s.length() && end < s.length()) {
+		while (end < s.length()) {
 			if (set.contains(s.charAt(end))) {
 				set.remove(s.charAt(start));
 				start++;
@@ -23,6 +25,25 @@ public class LongestSubstringWithoutRepeatingCharacters {
 			}
 		}
 		return res;
+	}
+	
+	public int lengthOfLongestSubstring2(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); 
+        
+        for (int j = 0, i = 0; i < n; i++) {
+            if (map.containsKey(s.charAt(i))) {
+            	// s.charAt(i) + 1 means after the duplicated character
+            	// j means the previous longest substring's starting point
+                j = Math.max(map.get(s.charAt(i)) + 1, j);
+            }
+            // put its current index
+            map.put(s.charAt(i), i);
+            
+            // calculate the new substring's distance.
+            ans = Math.max(ans, i - j + 1);
+        }
+        return ans;
 	}
 
 }
