6dda58a364047c82a1b7f17cf33d470bad3b5411
u0966537 <qq928090385@gmail.com>
2020-11-22 23:56:19 -0700

    finished problem 6

--- lc11182020/ZigZagConversion.java
+++ lc11182020/ZigZagConversion.java
@@ -2,52 +2,27 @@ package lc11182020;
 
 public class ZigZagConversion {
 
-	public static void main(String[]args) {
-		convert("PAYPALISHIRING", 3);
-	}
-	public static String convert(String s, int numRows) {
+	public String convert(String s, int numRows) {
 
 		if (numRows == 1) {
 			return s;
 		}
 
-		StringBuilder[] dp = new StringBuilder[numRows];
-		for (int i = 0; i < dp.length; i++) {
-			dp[i] = new StringBuilder();
-		}
-
-		for (int i = 0; i < s.length(); i++) {
-			int index = i % (2 * numRows - 2);
-			index = index < numRows ? index : 2 * numRows - 2 - index;
-			dp[index].append(s.charAt(i));
-		}
-
-		for (int i = 1; i < dp.length; i++) {
-			dp[0].append(dp[i]);
-		}
-
-		return dp[0].toString();
-
-	}
-
-	public static String convert2(String s, int numRows) {
-
-		if (numRows == 1)
-			return s;
-
-		StringBuilder ret = new StringBuilder();
+		StringBuilder sb = new StringBuilder();
 		int n = s.length();
-		int cycleLen = 2 * numRows - 2;
-
-		for (int i = 0; i < numRows; i++) {
-			for (int j = 0; j + i < n; j += cycleLen) {
-				ret.append(s.charAt(j + i));
-				if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
-					ret.append(s.charAt(j + cycleLen - i));
+		int formula = 2 * numRows - 2; // V sign.
+		
+		for(int row = 0; row < numRows; row++) {
+			for(int j = 0; row + j < n; j+= formula) {
+				sb.append(s.charAt(row + j));
+				// If i is not the first row and the last row,
+				// then look for jth formula.
+				if(row != 0 && row != numRows - 1 && j + formula - row < n ) {
+					sb.append(s.charAt(j + formula - row));
+				}
 			}
 		}
-
-		return ret.toString();
+		
+		return sb.toString();
 	}
-
 }
