fba14925840787d2db402b3bb38c4e515ab816fc
u0966537 <qq928090385@gmail.com>
2020-12-03 21:47:37 -0700

    finished problem 33

--- README.md
+++ README.md
@@ -138,3 +138,4 @@ Starting from 11/08/2020, I will review old problems and solve more problems. I 
 - 24 Swap Nodes in Pairs
 - 29 Divide Two Integers
 - 31 Next Permutation
+- 33 Search in Rotated Sorted Array
---
+++ lc12032020/SearchInRotatedSortedArray.java
@@ -0,0 +1,47 @@
+package lc12032020;
+
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0) {
+        	return -1;
+        }
+        
+        int left = 0, right = nums.length - 1;
+        
+        // at least 3 elements to perform
+        while(left + 1 < right) {
+        	// (right - left) / 2 = the center of left and right.
+        	int mid = left + (right - left) / 2;
+        	
+        	if(nums[mid] == target) {
+        		return mid;
+        	}
+        	
+        	if(nums[left] <= nums[mid]) {
+        		if(nums[left] <= target && target <= nums[mid]) {
+        			right = mid;
+        		}
+        		else {
+        			left = mid;
+        		}
+        	}
+        	else {
+        		if(nums[mid] <= target && target <= nums[right]) {
+        			left = mid;
+        		}else {
+        			right = mid;
+        		}
+        	}
+        }
+        
+        if(nums[left] == target) {
+        	return left;
+        }
+        else if(nums[right] == target) {
+        	return right;
+        }
+        else {
+        	return -1;
+        }
+     }
+}
