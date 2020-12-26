da7e61f50880caa4b1c5016254424f18f9ba17d3
何君君 <qq928090385@gmail.com>
2020-12-25 21:39:47 -0700

    finished problem 81

---
+++ lc12262020/SearchInRotatedSortedArrayll.java
@@ -0,0 +1,28 @@
+package lc12262020;
+
+public class SearchInRotatedSortedArrayll{
+	public boolean search(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return false;
+        
+        int left = 0, right = nums.length - 1;
+        
+        while(left + 1 < right){
+        	int mid = left + (right - left ) / 2;
+        	if(nums[mid] == target) return true;
+        	
+        	if(nums[mid] == nums[right]) right--;
+        	else if(nums[mid] == nums[left]) left++;
+        	else if(nums[left] < nums[mid]){
+        		if(nums[left] <= target && target <= nums[mid]) right = mid;
+        		else left = mid;
+        	}
+        	else if(nums[mid] < nums[right]){
+        		if(nums[mid] <= target && target <= nums[right]) left = mid;
+        		else right = mid;
+        	}
+        }
+        
+        if(nums[left] == target || nums[right] == target) return true;
+        else return false;
+    }
+}
